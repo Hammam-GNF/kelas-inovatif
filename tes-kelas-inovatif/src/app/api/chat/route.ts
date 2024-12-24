51105136ed919739f09e0c5e7a350278e7a78720
Hammam GNF <hammamgonjil@gmail.com>
2024-12-24 14:16:34 +0700

    install sdk-ai

--- tes-kelas-inovatif/src/app/api/chat/route.ts
+++ tes-kelas-inovatif/src/app/api/chat/route.ts
@@ -1,35 +1,14 @@
-import { Configuration, OpenAIApi } from "openai-edge";
-import { OpenAIStream, StreamingTextResponse } from "ai";
-
-// Create an OpenAI API client (that's edge friendly!)
-const config = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(config);
-
-// CORS headers
-const corsHeaders = {
-  "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-  "Access-Control-Allow-Headers": "Content-Type, Authorization",
-};
+import { CoreMessage, streamText } from "ai";
+import { openai } from "@ai-sdk/openai";
 
 export async function POST(req: Request) {
-  const { messages } = await req.json();
+  const { messages }: { messages: CoreMessage[] } = await req.json();
 
-  // Ask OpenAI for a streaming chat completion given the prompt
-  const response = await openai.createChatCompletion({
-    model: "gpt-4",
-    stream: true,
-    messages: messages.map((message: any) => ({
-      content: message.content,
-      role: message.role,
-    })),
+  const result = streamText({
+    model: openai("gpt-4"),
+    system: "You are a helpful assistant.",
+    messages,
   });
 
-  // Convert the response into a friendly text-stream
-  const stream = OpenAIStream(response);
-
-  // Respond with the stream
-  return new StreamingTextResponse(stream, { headers: corsHeaders });
+  return result.toDataStreamResponse();
 }
